4854dd8e3999c92e2c7515a71f8e939b974b9099
HassanSalah01 <hassan.salah.eldine1@gmail.com>
2022-05-25 11:50:43 +0200

    Doubly_Linked_list_Added_deleteExact_getLen

--- Doubly_Linked_List/Doubly_Linked_List.cpp
+++ Doubly_Linked_List/Doubly_Linked_List.cpp
@@ -6,7 +6,6 @@ Doubly_Linked_List::Doubly_Linked_List()
 {
     head = temp = NULL;
 }
-
 void Doubly_Linked_List::push(int val){
     Node* newNode = new Node ;
     newNode->value = val;
@@ -23,7 +22,6 @@ void Doubly_Linked_List::push(int val){
 }
 void Doubly_Linked_List::printAllNode(){
     Node* temp = head;
-    cout<<head<<endl;
     while(temp!=NULL){
         cout << temp->pre <<endl;
         cout << temp->value <<endl;
@@ -45,7 +43,6 @@ void Doubly_Linked_List::insertFirst(int val){
 
     }
 }
-
 void Doubly_Linked_List::insertAfter(int numb ,int val){
     Node* newNode = new Node;
     newNode->value = val;
@@ -59,9 +56,48 @@ void Doubly_Linked_List::insertAfter(int numb ,int val){
     newNode->next = temp->next;
     temp->next = newNode;
     
-    
+}
+void Doubly_Linked_List::deleteEnd(){
+    Node* temp = head;
+    while(temp->next->next!=NULL){
+        temp = temp->next;
+    }
+    delete temp->next;
+    temp->next = NULL;
+
 }
 
+void Doubly_Linked_List::deleteExactNode(int num){
+    if(num==1){
+        head=head->next;
+        delete head->pre;
+        head->pre = NULL;
+    }else if(getLen()>0 && num < getLen()){
+        Node* temp = head;
+        int numb = num-2;
+        while(numb>0){
+            temp =temp->next;
+            numb--;
+        }
+        cout<<temp<<endl;
+        Node* temp2 = temp->next->next;
+        temp2->pre = temp;
+        delete temp->next;
+        temp->next = temp2;
+        }
+    }
+
+
+int Doubly_Linked_List::getLen(){
+    Node* temp = head;
+    int count = 0 ;
+    while(temp!=NULL){
+        temp = temp->next;
+        count++;
+    }
+    return count;
+
+}
 Doubly_Linked_List::~Doubly_Linked_List()
 {
 
--- Doubly_Linked_List/main.cpp
+++ Doubly_Linked_List/main.cpp
@@ -12,10 +12,10 @@ int main(){
    d1.push(3);
    d1.push(4);
    d1.push(5);
-   d1.insertAfter(3,650);
+   d1.push(6);
+   d1.deleteExactNode(3);
    d1.printAllNode();
-//    d1.insertFirst(0);
-//    d1.printAllNode();
+   
 
 
     return 0;
